4254b0cc0c0ab0cc921b12ce5f42e9405636dcc6
Jose Mauro <developer@v8system.com>
2019-06-19 17:32:27 -0300

    feitos todos os calculos das faixas imc

--- lib/main.dart
+++ lib/main.dart
@@ -27,13 +27,27 @@ class _HomeState extends State<Home> {
   void calcula() {
     double weight = double.parse(weightController.text);
     double height = double.parse(heightController.text);
-    double imc = (weight / (height * height));
+    double imc = (weight / (height * height)*10000);
+
+    print(weight);
+    print(height);
+    print(imc);
 
     if (imc < 18.6) {
-      setState(() {
-        _infoText = "IMC ${imc} - Abaixo do Peso";
-      });
-    }
+      setState(() { 
+        _infoText = "IMC ${imc.toStringAsPrecision(4)} - Abaixo do Peso";
+        } );
+    } else if (imc >= 18.6 && imc < 24.9) {
+      setState(() {_infoText = "IMC ${imc.toStringAsPrecision(4)} - Peso ideal";});
+    } else if (imc >= 24.9 && imc < 29.9) {
+      setState(() {_infoText = "IMC ${imc.toStringAsPrecision(4)} - Levemente acima do peso";});
+    } else if (imc >= 29.9 && imc < 34.9) {
+      setState(() {_infoText = "IMC ${imc.toStringAsPrecision(4)} - Obesidade Grau I";});
+    } else if (imc >= 34.9 && imc < 39.9) {
+      setState(() {_infoText = "IMC ${imc.toStringAsPrecision(4)} - Obesidade Grau II";});
+    } else if (imc >= 40) {
+      setState(() {_infoText = "IMC ${imc.toStringAsPrecision(4)} - Obesidade Grau III";});
+    }  
   }
 
   @override
